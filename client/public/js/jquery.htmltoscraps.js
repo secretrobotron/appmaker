fb442ac852961b948121591700a25ace37f675ff
Breck <breck7@gmail.com>
2013-07-20 11:37:57 -0700

    Pulled latest htmltoscraps to start to fix #332 and other bugs

--- client/public/js/jquery.htmltoscraps.js
+++ client/public/js/jquery.htmltoscraps.js
@@ -1,3 +1,52 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/DOMParser
+/*
+ * DOMParser HTML extension
+ * 2012-09-04
+ * 
+ * By Eli Grey, http://eligrey.com
+ * Public domain.
+ * NO WARRANTY EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
+ */
+
+/*! @source https://gist.github.com/1129031 */
+/*global document, DOMParser*/
+
+(function(DOMParser) {
+	"use strict";
+
+	var
+	  DOMParser_proto = DOMParser.prototype
+	, real_parseFromString = DOMParser_proto.parseFromString
+	;
+
+	// Firefox/Opera/IE throw errors on unsupported types
+	try {
+		// WebKit returns null on unsupported types
+		if ((new DOMParser).parseFromString("", "text/html")) {
+			// text/html parsing is natively supported
+			return;
+		}
+	} catch (ex) {}
+
+	DOMParser_proto.parseFromString = function(markup, type) {
+		if (/^\s*text\/html\s*(?:;|$)/i.test(type)) {
+			var
+			  doc = document.implementation.createHTMLDocument("")
+			;
+	      		if (markup.toLowerCase().indexOf('<!doctype') > -1) {
+        			doc.documentElement.innerHTML = markup;
+      			}
+      			else {
+        			doc.body.innerHTML = markup;
+      			}
+			return doc;
+		} else {
+			return real_parseFromString.apply(this, arguments);
+		}
+	};
+}(DOMParser));
+
+
 $.fn.toSpace = function () {
   var space = new Space()
   var el = $(this)
@@ -35,9 +84,11 @@ $.fn.toSpace = function () {
 }
 
 $.htmlToScraps = function (html) {
-  var page = $(html)
+  
+  var doc = new DOMParser().parseFromString(html, "text/html")
   var space = new Space()
-  $(page).each(function () {
+  
+  $('html', doc).children().each(function () {
     // Skip whitespace
     if (!$(this).get(0).tagName)
       return true
@@ -48,12 +99,33 @@ $.htmlToScraps = function (html) {
 //      return true
     var num = 1
     var nextId = id
-    while (space.values[nextId]) {
+    while (space.get(nextId)) {
       num++
       nextId = id + num.toString()
     }
     var scrap = $(this).toSpace()
     space.set(nextId, scrap)
   })
+//  iframe.remove()
+
+// does html contain <head>?
+// does html contain <body>?
+//
+  if (html.match(/\<head\>/) && !html.match(/\<body\>/))
+    return space.delete('body')
+  
+  if (html.match(/\<head\>/) && html.match(/\<body\>/))
+    return space
+
+  if (!html.match(/\<head\>/) && !html.match(/\<body\>/)) {
+    space.delete('head')
+    return space.get('body scraps')
+  }
+  
+  if (!html.match(/\<head\>/) && html.match(/\<body\>/)) {
+    space.delete('head')
+    return space
+  }
+
   return space
 }
