36f038e562072c0fab70d1cf2ed8cf5b7bce3191
Breck <breck7@gmail.com>
2013-07-24 18:00:41 -0700

    refactor test script

--- client/tools/designer/tests.js
+++ client/tools/designer/tests.js
@@ -1,4 +1,7 @@
-var equal = function (a, b) {
+var Test = {}
+Test.tests = []
+
+Test.equal = function (a, b) {
   if (a === b)
     return console.log('PASS.')
   console.log('FAIL. We wanted %s but we got %s', a, b)
@@ -6,26 +9,38 @@ var equal = function (a, b) {
 //    debugger
 }
 
-console.log('Testing...')
-// Create a new page
-Designer.menu.create()
-// Stage should be empty
-equal(0, $('#DesignerStageBody').children().length)
+Test.add = function (module, fn) {
+  Test.tests.push(fn)
+}
 
+Test.start = function () {
+  console.log('Starting tests...')
+  Test.tests.forEach(function (value) {
+    value()
+  })
+}
 
+Test.add('test create', function () {
+  // Create a new page
+  Designer.menu.create()
+  // Stage should be empty
+  Test.equal(0, $('#DesignerStageBody').children().length)
+})
 
 // Paste
-Designer.menu.create()
-// Stage should be empty
-equal(0, $('#DesignerStageBody').children().length)
-Designer.pasteHtml('<a>Hello world</a>')
-// Stage should have 1 element
-equal(1, $('#DesignerStageBody').children().length)
+Test.add('paste html', function () {
+  Designer.menu.create()
+  // Stage should be empty
+  Test.equal(0, $('#DesignerStageBody').children().length)
+  Designer.pasteHtml('<a>Hello world</a>')
+  // Stage should have 1 element
+  Test.equal(1, $('#DesignerStageBody').children().length)
+})
 
+Test.add('https://github.com/nudgepad/nudgepad/issues/332', function () {
+  Designer.menu.create()
+  Designer.pasteHtml('<head><title id="title">Hello world</title></head>')
+  Test.equal($('#Test332').text(), Designer.page.toString())  
+})
 
-// https://github.com/nudgepad/nudgepad/issues/332
-Designer.menu.create()
-Designer.pasteHtml('<head><title id="title">Hello world</title></head>')
-equal($('#Test332').text(), Designer.page.toString())
-
-
+Test.start()
