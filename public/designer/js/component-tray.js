7901ed92046031cb2e11fc82594252ee7477dcf8
secretrobotron <secretrobotron@gmail.com>
2014-06-16 18:17:58 -0400

    Pre-load user components before Polymer
    Uses AJAX to load components before making a link tag for them. Components are lifted from text of comopnent.htmls that don't start with <polymer-element>.

--- public/designer/js/component-tray.js
+++ public/designer/js/component-tray.js
@@ -8,7 +8,13 @@ define(
     "use strict";
 
     var resolvePath = function(tag, url) {
-      return document.createElement(tag).resolvePath(url);
+      try {
+        return document.createElement(tag).resolvePath(url);
+      }
+      catch (e) {
+        console.error('Could not resolve path for ', tag);
+        return url;
+      }
     };
 
     var knownComponents = [];
